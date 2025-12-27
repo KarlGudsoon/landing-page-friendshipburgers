28c63f2b5ccae838e54b6ac85ea2241fec094ed2
KarlGudsoon <maturana.or.adrian@gmail.com>
2025-12-27 16:23:41 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
-import Navbar from "./components/navbar.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 export default function App() {
   return (
