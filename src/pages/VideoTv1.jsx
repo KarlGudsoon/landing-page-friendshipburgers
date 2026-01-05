2965a8a1ce731a96e0a3bd0442c5fc9838d57467
KarlGudsoon <maturana.or.adrian@gmail.com>
2026-01-05 00:38:10 -0300

    cambios varios

--- src/pages/VideoTv1.jsx
+++ src/pages/VideoTv1.jsx
@@ -1,24 +1,14 @@
-import { useRef } from "react";
-
 export default function Video1() {
-  const videoRef = useRef(null);
-
-  const goFullscreen = () => {
-    videoRef.current.requestFullscreen();
-    videoRef.current.play();
-  };
   return (
-    <main
-      onClick={goFullscreen}
-      className="w-screen h-screen overflow-hidden bg-black"
-    >
+    <main className="w-screen h-screen overflow-hidden bg-black">
       <video
-        ref={videoRef}
         src="/video/tv1.mp4"
         muted
         loop
         controls={false}
         playsInline
+        autoPlay
+        preload="true"
         className="w-full h-full"
       />
     </main>
--- src/pages/VideoTv2.jsx
+++ src/pages/VideoTv2.jsx
@@ -1,24 +1,14 @@
-import { useRef } from "react";
-
 export default function Video1() {
-  const videoRef = useRef(null);
-
-  const goFullscreen = () => {
-    videoRef.current.requestFullscreen();
-    videoRef.current.play();
-  };
   return (
-    <main
-      onClick={goFullscreen}
-      className="w-screen h-screen overflow-hidden bg-black"
-    >
+    <main className="w-screen h-screen overflow-hidden bg-black">
       <video
-        ref={videoRef}
         src="/video/tv2.mp4"
-        autoPlay
         muted
         loop
+        controls={false}
         playsInline
+        autoPlay
+        preload="true"
         className="w-full h-full"
       />
     </main>
